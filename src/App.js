276e2c598600a7dcb67869c42890d551d84e4567
Mike Wong <wongmdesign@gmail.com>
2019-08-23 14:49:59 -0400

    view layer renders state from redux store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import TasksPage from './components/TasksPage'
 
-export default class App extends Component {
+class App extends Component {
   render () {
     return (
       <div className='maincontent'>
-        <TasksPage tasks={mockTasks} />
+        {/* tasks will be available via props after connected to the store */}
+        <TasksPage tasks={this.props.tasks} />
       </div>
     )
   }
 }
+// state argument is the entire content of the redux store, (calling getState on the store instance)
+// return value of mapStateToProps is passed into the App component as props, which is why render can reference this.props.tasks
+const mapStateToProps = state => {
+  return {
+    tasks: state.tasks
+  }
+}
+
+export default connect(mapStateToProps)(App)
